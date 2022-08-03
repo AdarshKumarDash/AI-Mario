38f852c3d88084c1f4caa1a1e4e89a07ab6913f5
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-08-03 19:56:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+	video.parent("game_mario");
 }
 
 function draw() {
