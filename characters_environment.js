ab8fa49c2848723fb308dee45c89e053f4bd6003
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-08-24 17:24:34 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -59,7 +59,7 @@ game_status = "";
 
 function start() {
   game_status = "Start";
-  document.getElementById("status").innerHTML = "Game is Loading";
+  document.getElementById("status").innerHTML = "Game is loading";
 }
 
 function game(){
@@ -77,7 +77,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press start button to play", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -125,8 +125,9 @@ function game(){
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if((game_status == "Start") && nosex != "" && gameConfig.status==="start") {
     world_start.play();
+    document.getElementById("status").innerHTML = "Game loaded";
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
   }
@@ -289,13 +290,13 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(nosex < 300){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(nosex > 300){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
@@ -310,7 +311,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (nosey < 200 && character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
